8f6dc8e8e0a50ff6125f3e975aa02ea4657b84e9
David Shupe <shupe@ipac.caltech.edu>
2023-02-09 16:03:19 -0800

    add load_nddata, zoom, center_on

--- firefly-astrowidgets/core.py
+++ firefly-astrowidgets/core.py
@@ -11,7 +11,9 @@ import numpy as np
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
+from astropy.nddata import CCDData, fits_ccddata_writer
 from astropy.table import Table, vstack
+import astropy.units as u
 from astropy.utils.decorators import deprecated
 from astropy.utils.exceptions import AstropyWarning
 
@@ -62,15 +64,18 @@ class FireflyWidget:
     """
 
     def __init__(self, start_browser_tab=False, *args, **kwargs):
-        
+
         html_file = kwargs.get('html_file',
                                os.environ.get('FIREFLY_HTML', 'slate.html'))
-        
+
         self._viewer = firefly_client.FireflyClient.make_lab_client(
                 start_tab=True, html_file=html_file)
 
         if start_browser_tab:
             self._viewer.launch_browser()
+
+        self._zlevel = 1.0
+        self._pixel_offset = 1
 
 
     def load_fits(self, fitsorfn, numhdu=None, memmap=None):
@@ -97,18 +102,48 @@ class FireflyWidget:
         if isinstance(fitsorfn, str):
             if numhdu == 0 or numhdu is None:
                 fname = fitsorfn
- 
+                temp = False
+
             else:
                 hdulist = fits.open(fitsorfn)
                 hdu = hdulist[numhdu]
                 fname = self._write_temp_fits(hdu)
+                temp = True
 
         elif isinstance(fitsorfn, (fits.ImageHDU, fits.CompImageHDU,
                                    fits.PrimaryHDU)):
             fname = self._write_temp_fits(fitsorfn)
-        
+            temp = True
+
         f = self._viewer.upload_file(fname)
-        self._viewer.show_fits(f)
+        self._viewer.show_fits(f, plot_id='main')
+        if temp:
+            os.remove(fname)
+
+    def load_nddata(self, nddata):
+        """
+        Load an ``NDData`` object into the viewer.
+
+        .. todo:: Add flag/masking support, etc.
+
+        Parameters
+        ----------
+        nddata : `~astropy.nddata.NDData`
+            ``NDData`` with image data and WCS.
+
+        """
+        if nddata.unit is None:
+            unit = 'DN'
+        else:
+            unit = nddata.unit
+        ccd = CCDData(nddata, unit=unit)
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.fits') as fd:
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', AstropyWarning)
+                fits_ccddata_writer(ccd, fd.name)
+        f = self._viewer.upload_file(fd.name)
+        self._viewer.show_fits(f, plot_id='main')
+        os.remove(fd.name)
 
     @property
     def zoom_level(self):
@@ -120,14 +155,12 @@ class FireflyWidget:
         * 0.5 means zoomed out by a factor of 2.
 
         """
-        return self._viewer.get_scale()
+        return self._zlevel
 
     @zoom_level.setter
     def zoom_level(self, val):
-        if val == 'fit':
-            self._viewer.zoom_fit()
-        else:
-            self._viewer.scale_to(val, val)
+        self._zlevel = val
+        self._viewer.set_zoom(plot_id='main', factor=val)
 
     def zoom(self, val):
         """
@@ -142,8 +175,27 @@ class FireflyWidget:
         """
         self.zoom_level = self.zoom_level * val
 
+    def center_on(self, point):
+        """
+        Centers the view on a particular point.
 
-    def _write_temp_fits(hdu):
+        Parameters
+        ----------
+        point : tuple or `~astropy.coordinates.SkyCoord`
+            If tuple of ``(X, Y)`` is given, it is assumed
+            to be in data coordinates.
+        """
+        if isinstance(point, SkyCoord):
+            self._viewer.set_pan(plot_id='main',
+                                 x=point.ra.deg, y=point.dec.deg,
+                                 coord='J2000')
+        else:
+            self._viewer.set_pan(plot_id='main',
+                                 x=point[0] - self._pixel_offset + 1,
+                                 y=point[1] - self._pixel_offset + 1,
+                                 coord='image')
+
+    def _write_temp_fits(self, hdu):
         """
         Write a FITS HDU to a temporary location.
 
@@ -161,8 +213,8 @@ class FireflyWidget:
         with tempfile.NamedTemporaryFile(delete=False, suffix='.fits') as fd:
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore', AstropyWarning)
-                hdu.writeto(fd, overwrite=True)
-        return(fd)
+                hdu.writeto(fd.name, overwrite=True)
+        return(fd.name)
 
 
 def _offset_is_pixel_or_sky(x):
